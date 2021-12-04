55615f5ed84dc83ee5bd6aeeffbf38a9744b44be
AnnaRabychina <nutochka@tut.by>
2021-12-05 01:01:09 +0300

    feat: add interfaces for data exchange with the News API

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-  constructor() {
-    super('https://newsapi.org/v2/', {
-      apiKey: 'ef20ba39e5184e19918fb9442763df7b',
-    });
-  }
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: 'ef20ba39e5184e19918fb9442763df7b', // получите свой ключ https://newsapi.org/
+        });
+    }
 }
 
 export default AppLoader;
--- migration-to-typescript/src/components/view/appView.ts
+++ migration-to-typescript/src/components/view/appView.ts
@@ -1,21 +1,58 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+interface ISource {
+    id: string;
+    name: string;
+  }
+
+export interface IArticle {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+    source: ISource;
+  }
+
+export interface IApiNews {
+    status: string;
+    totalResults: number;
+    articles: IArticle[];
+  }
+
+  export interface ISources extends ISource{
+    category: string;
+    country: string;
+    description: string;
+    url: string;
+    language: string;
+  }
+  
+  export interface IApiSources{
+    status: string;
+    sources: ISources[];
+  }
+
 export class AppView {
-  constructor() {
-    this.news = new News();
-    this.sources = new Sources();
-  }
+    news: News;
+    sources: Sources;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
 
-  drawNews(data) {
-    const values = data?.articles ? data?.articles : [];
-    this.news.draw(values);
-  }
+    drawNews(data: IApiNews) {
+        const values = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
 
-  drawSources(data) {
-    const values = data?.sources ? data?.sources : [];
-    this.sources.draw(values);
-  }
+    drawSources(data: IApiSources) {
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
 }
 
 export default AppView;
